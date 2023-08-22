1f94fdc6ba0aa30d9b3f699c535095dafcc34aaa
Deluan26 <deluan19951@gmail.com>
2023-08-22 17:41:03 -0300

    att

---
+++ src/main.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Footer from './components/Footer'
+import Header from './components/Header'
+// import App from './App'
+import './index.css';
+import ListaServicos from './pages/ListaServicos';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import ListaDevs from './pages/ListaDevs';
+import PerfilUsuario from './pages/PerfilUsuario';
+import VisualizarServico from './pages/VisualizarServico';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='ListaServicos' element={<ListaServicos/>} />
+        <Route path='ListaDevs' element={<ListaDevs/>} />
+        <Route path='Perfil/:idUsuario' element= {<PerfilUsuario/>} />
+        <Route path='Visualizar/Servico' element= {<VisualizarServico/>} />
+      </Routes>
+      </BrowserRouter>
+
+</React.StrictMode>
+)
